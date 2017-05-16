ecd0d31709058f3c61e1ad465ae5b76d67041c45
Ryan Hawkins <RyanHawkins@u.boisestate.edu>
2017-05-16 09:28:11 -0700

    Counts reports on one page and reports number

--- urlTry.py
+++ urlTry.py
@@ -3,9 +3,14 @@ from selenium import webdriver
 import time  
 browser = webdriver.Firefox()  
 browser.get('https://hackerone.com/hacktivity?sort_type=latest_disclosable_activity_at&filter=type%3Apublic&page=1&range=forever')  
-time.sleep(1)  
+time.sleep(3)  
 print(browser.title) 
-print(browser.find_elements_by_class_name('hacktivity__wrapper')) 
+ids = browser.find_elements_by_class_name('hacktivity__wrapper')
+num = 0
+for ii in ids:
+
+    num = num + 1
+print(num)     
 browser.quit() 
 
 
