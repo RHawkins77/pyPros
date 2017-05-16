082e0050fd6911399cd882e07d07da75e8e8c469
Ryan Hawkins <ryanhawkins@u.boisestate.edu>
2017-05-16 16:40:57 -0700

    pushing before power outage

--- urlTry.py
+++ urlTry.py
@@ -1,12 +1,16 @@
 from requests import session
-from selenium import webdriver  
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 import time  
+
 #open Web Browser
 browser = webdriver.Firefox()  
 #get hackerone disclosed hacktivity page
-browser.get('https://hackerone.com/hacktivity?sort_type=latest_disclosable_activity_at&filter=type%3Apublic&page=1&range=forever')  
+browser.get('https://hackerone.com/hacktivity?sort_type=latest_disclosable_activity_at&filter=type%3Apublic&page=139&range=forever')  
 #wait for 3 seconds for page to load
-time.sleep(3)
+time.sleep(1)
 #print pages title  
 print(browser.title)
  
@@ -18,12 +22,13 @@ for ii in ids:
     num = num + 1
 
 while myButton.is_enabled():
-    myButton.click()
-    myButton = browser.find_element_by_css_selector('button.button:nth-child(2)')
-    time.sleep(3)
-    ids2 = browser.find_elements_by_class_name('hacktivity__wrapper')
+    myButton.click()     
+    time.sleep(1)
+    myButton = browser.find_element_by_xpath('/html/body/div[3]/span/div/div[2]/div[1]/div[27]/div[1]/div[2]/button[2]')
+    ids2 = myButton.find_elements_by_class_name('hacktivity__wrapper')
     for ii in ids2:
         num = num + 1
+    if myButton.is
 
 print(num)
 browser.quit() 
