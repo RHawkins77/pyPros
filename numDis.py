3452363c4648c400dfe24cdc903c93132c939973
Ryan Hawkins <RyanHawkins@u.boisestate.edu>
2017-05-18 14:35:38 -0700

    numDis is Number of Disclosed python file, urlTry is what I am working on right now, not a working copy at this moment

--- numDis.py
+++ numDis.py
@@ -9,28 +9,33 @@ import time
 #open Web Browser
 browser = webdriver.Firefox()  
 #get hackerone disclosed hacktivity page
-browser.get('https://hackerone.com/hacktivity?sort_type=upvotes&filter=type%3Apublic&page=139&range=forever')  
+browser.get('https://hackerone.com/hacktivity?sort_type=upvotes&filter=type%3Apublic&page=1&range=forever')  
 #wait for 3 seconds for page to load
 time.sleep(2)
 #print pages title  
 print(browser.title)
  
-ids = browser.find_elements_by_class_name('hacktivity__wrapper').text()
+ids = browser.find_elements_by_class_name('hacktivity__wrapper')
 num = 0
 
 while True:
     myButton = browser.find_element_by_xpath('/html/body/div[3]/span/div/div[2]/div[1]/div[27]/div[1]/div[2]/button[2]')
-    ids.append(browser.find_elements_by_class_name('hacktivity__wrapper'))
-    myButton.click()   
+    for ii in ids:
+        num = num + 1
+    myButton.click()     
     time.sleep(2)
     try:	    
-        myButton = browser.find_element_by_xpath('/html/body/div[3]/span/div/div[2]/div[1]/div[27]/div[1]/div[2]/button[2]')
-        ids.append(browser.find_elements_by_class_name('hacktivity__wrapper'))
+	myButton = browser.find_element_by_xpath('/html/body/div[3]/span/div/div[2]/div[1]/div[27]/div[1]/div[2]/button[2]')
+        ids2 = myButton.find_elements_by_class_name('hacktivity__wrapper')
+        for ii in ids2:
+            num = num + 1
     except NoSuchElementException:
-        ids.append(browser.find_elements_by_class_name('hacktivity__wrapper')
-        break
+        ids3 = browser.find_elements_by_class_name('hacktivity__wrapper')
+        for ii in ids3	:
+            num = num + 1
+	break
 
-print(ids)
+print(num)
 browser.quit() 
 
 #with session() as c:
